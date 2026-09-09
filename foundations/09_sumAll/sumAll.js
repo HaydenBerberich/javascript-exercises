1b22eeff86f46cb2b1deb14c2ee3c0f82af04fcd
Hayden Berberich <haydenberberich@gmail.com>
2026-09-09 11:57:44 -0500

    Complete foundations 6-10

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if ((!Number.isInteger(a) || a <= 0) || (!Number.isInteger(b) || b <= 0)) return `ERROR`;
 
+    let low = 0;
+    let high = 0;
+
+    if (a < b) {
+        low = a;
+        high = b;
+    } else {
+        low = b;
+        high = a;
+    }
+
+    let sum = 0;
+    for (let i = low; i <= high; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
